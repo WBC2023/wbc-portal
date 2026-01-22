0cc45939bc261054c25b9376a2f1d1ca3353ffc5
WBC2023 <wbca2023@gmail.com>
2026-01-22 16:54:22 +0800

    Update Chatbot.tsx

--- Chatbot.tsx
+++ Chatbot.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useRef, useEffect } from 'react';
-import { askInsuranceAssistant } from './services/geminiService';
+import { askInsuranceAssistant } from '.geminiService';
 
 export const Chatbot: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
